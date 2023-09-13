5a3c32a821103587f10eea6a46b271427d092731
YTDavid93 <ytd3809@gmail.com>
2023-09-13 10:29:37 +0545

    Added .env to .gitignore

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,3 @@
 </template>
 
 <style scoped></style>
-    // const {table_id} = data
-    // locations.value.map((location) => {
-    //   location.tables((table) => {
-    //     if (table.id === table_id) {
-    //       table.occupied = data.occupied
-    //     }
-    //   })
-    // })
--- src/Socket/socketStore.js
+++ src/Socket/socketStore.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client'
-import { ref } from 'vue'
 
 let socketDefault = null
 
